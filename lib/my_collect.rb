680487dd8a7241b66644e51c9b45a2e0daee858e
Megan Schertz <meganschertz@gmail.com>
2020-06-26 20:36:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+require 'pry'
+def my_collect(array)
+    collection = []
+    counter = 0
+    while counter < array.length 
+        collection << (yield array[counter])
+        counter += 0
+    end
+    collection
+   
+end
 
